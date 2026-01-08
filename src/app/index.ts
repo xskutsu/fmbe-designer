972cf87e9182faa337081dab3bb955416a83986a
xskutsu <xsku@protonmail.com>
2026-01-08 13:44:32 -0600

    Implement standard molang variables

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import { OrbitCameraControl } from "./input/orbitCameraControl";
 import { Editor } from "./live/editor";
+import type { MolangVariableMap } from "./molang/types";
 import { Viewport } from "./render/view/viewport";
 
 const editor: Editor = new Editor();
@@ -9,9 +10,18 @@ viewport.onresize = () => viewport.render(editor.scene);
 
 const cameraControl = new OrbitCameraControl(viewport.camera, viewport.renderer.domElement);
 
+const molangVariables: MolangVariableMap = new Map([
+	["q.life_time", () => performance.now() / 1000],
+	["query.life_time", () => performance.now() / 1000],
+	["q.vertical_speed", () => 0],
+	["query.vertical_speed", () => 0],
+	["q.modified_distance_moved", () => 0],
+	["query.modified_distance_moved", () => 0]
+]);
+
 function animateFrame(): void {
 	cameraControl.update();
-	editor.update(null);
+	editor.update(molangVariables);
 	viewport.render(editor.scene);
 	requestAnimationFrame(animateFrame);
 }
